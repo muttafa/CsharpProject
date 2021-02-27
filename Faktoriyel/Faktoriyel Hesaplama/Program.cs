1b247f67142689476ac9d0110545786179e72a19
muttafa <mucarr419@gmail.com>
2021-02-27 16:49:43 +0300

    Faktoriyel Hesaplama
    C# ile faktoriyel Hesaplama

---
+++ Faktoriyel/Faktoriyel Hesaplama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Faktoriyel_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Faktoriyeli hesaplanacak olan sayıyı giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = 1;
+            for (int i = sayi ; i> 0; i--)
+            {
+                
+                faktoriyel *= i;
+                
+            }
+            Console.WriteLine("{0} sayisinin faktoriyeli :{1}",sayi,faktoriyel);
+        }
+    }
+}
