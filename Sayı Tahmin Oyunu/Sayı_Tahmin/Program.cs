cd9860f94d2ef7fbfdd2b5e79d689289e7fad0e4
muttafa <mucarr419@gmail.com>
2021-02-27 17:02:53 +0300

    Sayı Tahmin Oyunu
    C# ile Sayı Tahmin Oyunu

---
+++ Sayı Tahmin Oyunu/Sayı_Tahmin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sayı_Tahmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int puan = 0;
+            Console.WriteLine("Bir ile 100 arasında bir sayı girin :");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int pc = rnd.Next(1, 100);
+
+            if (pc == sayi)
+            {
+                Console.WriteLine("!!!Tebrikler Sayıyı bildiniz!!!");
+                puan++;
+            }
+            else
+            {
+                Console.WriteLine("Bilemediniz...Sayı :"+pc);
+                Console.WriteLine("puanınız :"+puan);
+            }
+        }
+    }
+}
