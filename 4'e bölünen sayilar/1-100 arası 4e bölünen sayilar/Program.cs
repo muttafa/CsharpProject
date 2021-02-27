70d06a08124e2f235db3862900c407db98ffdb0d
muttafa <mucarr419@gmail.com>
2021-02-27 16:39:40 +0300

    CsharpProject
    C# ile yazdığım projeler

---
+++ 4'e bölünen sayilar/1-100 arası 4e bölünen sayilar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_100_arası_4e_bölünen_sayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i%4==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Not Takip Programı/Not_Takip/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Not_Takip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu projede öğrencinin dersten geçip geçmediğini hesaplayan basit bir program yazdım...
+            Console.WriteLine("Merhaba Lütfen Kısa Sınav Puanınızı Giriniz : ");
+            int ks1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen 2.Kısa Sınav Puanınızı Giriniz : ");
+            int ks2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen Vize Sınav Puanınızı Giriniz : ");
+            int vize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen Final Sınav Puanınızı Giriniz : ");
+            int final = Convert.ToInt32(Console.ReadLine());
+
+            double ortalama = (ks1 / 10 + ks2 / 10 + (3 * vize / 10) + final / 2);
+            Console.WriteLine("Ortalamanız :"+ortalama);
+            double harf = ortalama / 25;
+            switch (harf)
+            {
+                case 1: Console.WriteLine("Harf notunuz ff dir .. KALDINIZ!!");
+                    break;
+                case 1.5: Console.WriteLine("Harf notunuz dd dir.Kıl payı geçtiniz");
+                    break;
+                case 2: Console.WriteLine("Harf notunuz cc dir.");
+                    break;
+                case 3: Console.WriteLine("Harf notunuz bb dir.");
+                    break;
+                case 4: Console.WriteLine("Harf notunuz aa dir.");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
